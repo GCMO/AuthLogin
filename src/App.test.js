b65e029fb23805212cd20fc6e2096af682bbaf1c
GCMO <g.c.m.orlandi@gmail.com>
2023-01-09 17:17:09 +0100

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,7 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 import React from 'react';
 import Login from './pages/Login';
-import { shallow, configure, mount, render } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -20,22 +12,6 @@ describe('Login Form', () => {
     expect(wrapper.find('form').length).toBe(1);
   });
 
-  it('should render two input fields and a submit button', () => {
-    const wrapper = shallow(<form />);
-    expect(wrapper.find('input').length).toBe(2);
-    expect(wrapper.find('button').length).toBe(1)
-  });
-
-  it('should update the state when the input fields are changed', () => {
-    const wrapper = shallow(<form />);
-    const emailInput = wrapper.find('input').at(0);
-    const passwordInput = wrapper.find('input').at(1);
-    emailInput.simulate('change', { target: { value: 'test' } });
-    passwordInput.simulate('change', { target: { value: 'password' } });
-    expect(wrapper.state('email')).toBe('test');
-    expect(wrapper.state('password')).toBe('password');
-  });
-
   it('should send a POST request to the server when the form is submitted', () => {
     const wrapper = shallow(<form />);
     const form = wrapper.find('form');
@@ -43,3 +19,19 @@ describe('Login Form', () => {
     // test that a POST request was sent to the server
   });
 });
+
+// it('should render two input fields and a submit button', () => {
+//   const wrapper = shallow(<Login />);
+//   expect(wrapper.find('input').length).toBe(2);
+//   expect(wrapper.find('button').length).toBe(1)
+// });
+
+// it('should update the state when the input fields are changed', () => {
+//   const wrapper = shallow(<form />);
+//   const emailInput = wrapper.find('input').at(0);
+//   const passwordInput = wrapper.find('input').at(1);
+//   emailInput.simulate('change', { target: { value: 'test' } });
+//   passwordInput.simulate('change', { target: { value: 'password' } });
+//   expect(wrapper.state('email')).toBe('test');
+//   expect(wrapper.state('password')).toBe('password');
+// });
